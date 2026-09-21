a0d9c66feb705c480f069f2ddb756f0989ade976
Lehtung <143353274+Lehtung@users.noreply.github.com>
2026-09-21 11:24:58 +0700

    Update cau-hinh.js

--- js/khung/cau-hinh.js
+++ js/khung/cau-hinh.js
@@ -2,6 +2,6 @@
    API_URL: địa chỉ Web App của Google Apps Script (Deploy → Manage deployments → Web app URL).
    Triển khai ngày 21/09/2026. Khi triển khai lại phiên bản mới của CÙNG một lần triển khai, URL không đổi. */
 window.CAU_HINH_WEB = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwcAeHeev9ffjNBH0UIxHAWaPEPuHJ6XlZsJ12v5KHXdd0NIlxsZ0U6UJqAskkRGeWB/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzXsrivzOD5XVJiyENwwP1y74S86efDAmZJnRXgM-kV39MQDr9uE2rO1EWb3o8vMQO0/exec',
   THOI_GIAN_CHO_MS: 90000
 };
