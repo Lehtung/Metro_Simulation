38de5bf00479afc7155ab0a600faf082252ef21c
Lehtung <143353274+Lehtung@users.noreply.github.com>
2026-09-22 19:52:29 +0700

    Update cau-hinh.js

--- js/khung/cau-hinh.js
+++ js/khung/cau-hinh.js
@@ -1,6 +1,6 @@
 /* @file js/khung/cau-hinh.js — cấu hình bản web
    API_URL: địa chỉ Web App của Google Apps Script (Deploy → Manage deployments → Web app URL). */
 window.CAU_HINH_WEB = {
-  API_URL: 'https://script.google.com/macros/s/AKfycby2ADG3FFDsKKnbwZ0ylpKjKLhmp9BTfxc2YFXib5j6KPhuZHPkHRRl3iY1UAbFAsEs/exec',
+  API_URL: 'https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQVZmI7iY8xOhsSAMGggQUMKCNcfRLRaBf9gRWjRA7OhlxbfPtiYDQXUVgAq2lE6pRf4ZJLp4zA6akN1KL0grlkYhUZjkY8blKp5h9rVbfatWmftsiPKpdYyBKVZWUxFMfzXzWx5DqWyLffXdolK1fqwS7d5HhhP7SnqdwnwVOL9wux2wXs6arVaB2TAp-fopZBr6V9ZuZEbRosSUkw-wV_L83aHxK91uwQ9LXVX1DDLjlv6t45OIh-eA3wB7VXdM_C4k_7zcKqjF1-33eceb9Y_J4IPQ&lib=MnhNKK93ACYXqgxxU27D2Gc6idZU4jP8o',
   THOI_GIAN_CHO_MS: 90000
 };
